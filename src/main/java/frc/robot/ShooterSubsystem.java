9da6d8191b8bc95a1ef262b7c6b406bababc6dc8
alexarvin6 <97808017+alexarvin6@users.noreply.github.com>
2022-02-22 20:17:56 -0500

    Update ShooterSubsystem.java

--- src/main/java/frc/robot/ShooterSubsystem.java
+++ src/main/java/frc/robot/ShooterSubsystem.java
@@ -1,12 +1,11 @@
 package java.frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
-  public CANSparkMax shooterMotor1 = new CANSparkMax(Constants.shooterMotor1_ID, MotorType.kBrushless);
-  public CANSparkMax shooterMotor2 = new CANSparkMax(Constants.shooterMotor2_ID, MotorType.kBrushless);
+  public Spark shooterMotor1 = new Spark(Constants.shooterMotor1Channel);
+  public Spark shooterMotor2 = new Spark(Constants.shooterMotor2Channel);
   
   shooterMotor2.follow(shooterMotor1);
   
