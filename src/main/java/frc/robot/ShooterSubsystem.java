4208c54b9c0e00f14d3c381c54d214cfd335c92b
alexarvin6 <97808017+alexarvin6@users.noreply.github.com>
2022-02-22 19:28:46 -0500

    Update ShooterSubsystem.java

--- src/main/java/frc/robot/ShooterSubsystem.java
+++ src/main/java/frc/robot/ShooterSubsystem.java
@@ -8,8 +8,17 @@ public class ShooterSubsystem extends SubsystemBase {
   public CANSparkMax shooterMotor1 = new CANSparkMax(Constants.shooterMotor1_ID, MotorType.kBrushless);
   public CANSparkMax shooterMotor2 = new CANSparkMax(Constants.shooterMotor2_ID, MotorType.kBrushless);
   
+  shooterMotor2.follow(shooterMotor1);
+  
   @Override
   public void periodic() {
-  
+    int[] motorSpeeds = {.1, .2, .4, .5, .7, .8, 1};
+    
+    if (operatorStick.getRawButton(2)) {
+      for (i = 0; i < motorSpeeds.length; i++) {
+        shooterMotor1.set(i);
+        wait(715);
+      }
+    }
   }
 }
