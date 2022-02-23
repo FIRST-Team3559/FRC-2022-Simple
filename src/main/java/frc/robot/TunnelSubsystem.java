4f854b5fc55bf8080c765538b2792cc2dbc4e980
alexarvin6 <97808017+alexarvin6@users.noreply.github.com>
2022-02-22 20:17:52 -0500

    Update TunnelSubsystem.java

--- src/main/java/frc/robot/TunnelSubsystem.java
+++ src/main/java/frc/robot/TunnelSubsystem.java
@@ -4,14 +4,13 @@ import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TunnelSubsystem extends SubsystemBase {
-  public Spark bottomTunnelMotor = new Spark(Constants.bottomTunnelChannel);
-  public Spark topTunnelMotor = new Spark(Constants.topTunnelChannel);
+  public Spark ballTunnelMotor = new Spark(Constants.ballTunnelChannel);
   
   public void tunnel() {
     if (operatorStick.getRawButton(3)) {
-      ballTunnel.set(-.7);
+      ballTunnelMotor.set(-.7);
     } else {
-      ballTunnel.set(0);
-   }
+      ballTunnelMotor.set(0);
+    }
   }
 }
